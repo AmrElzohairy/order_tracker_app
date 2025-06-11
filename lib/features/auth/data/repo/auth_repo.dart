fae4e0ea8c7b01991521712be027a401bdfd1e7b
amrElzohairy <aelzohairy9@gmail.com>
2025-06-11 20:49:21 +0300

    refactors in register screen

---
+++ lib/features/auth/data/repo/auth_repo.dart
@@ -0,0 +1,14 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// class AuthRepo {
+//   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+//   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+//   Future<Either<String,String>> registerUser({
+//     required String username,
+//     required String email,
+//     required String password
+//   }){}
+// }
