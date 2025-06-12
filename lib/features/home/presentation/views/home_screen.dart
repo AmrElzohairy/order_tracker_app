9e298bafff6a0b6d2bbd50a9ea248e84ede85b36
amrElzohairy <aelzohairy9@gmail.com>
2025-06-12 12:40:28 +0300

    add orders container

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:order_tracker_app/core/styling/app_colors.dart';
+import 'package:order_tracker_app/core/styling/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        centerTitle: true,
+        title: Text(
+          "Home",
+          style: AppStyles.primaryHeadLinesStyle.copyWith(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+        ),
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor.withValues(alpha: 0.9),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Text(
+                  "Orders",
+                  style: AppStyles.primaryHeadLinesStyle.copyWith(
+                    fontSize: 18,
+                    color: AppColors.whiteColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor.withValues(alpha: 0.9),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Text(
+                  "Add Order",
+                  style: AppStyles.primaryHeadLinesStyle.copyWith(
+                    fontSize: 18,
+                    color: AppColors.whiteColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
