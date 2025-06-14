5df146394b0c988c1330a6bb9840b779c8dda261
amrElzohairy <aelzohairy9@gmail.com>
2025-06-14 19:51:39 +0300

    add order cubit

--- lib/features/order/presentation/cubits/order_cubit/order_cubit.dart
+++ lib/features/order/presentation/cubits/order_cubit/order_cubit.dart
@@ -1,8 +1,20 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:order_tracker_app/features/order/data/model/order_model.dart';
+import 'package:order_tracker_app/features/order/data/repo/order_repo.dart';
 
 part 'order_state.dart';
 
 class OrderCubit extends Cubit<OrderState> {
-  OrderCubit() : super(OrderInitial());
+  OrderCubit({required this.orderRepo}) : super(OrderInitial());
+  final OrderRepo orderRepo;
+
+  Future<void> addOrder({required OrderModel orderModel}) async {
+    emit(OrderAddingStatus());
+    final result = await orderRepo.addOrder(orderModel: orderModel);
+    result.fold(
+      (error) => emit(OrderAddedError(error)),
+      (success) => emit(OrderAddedSuccess(success)),
+    );
+  }
 }
--- lib/features/order/presentation/cubits/order_cubit/order_state.dart
+++ lib/features/order/presentation/cubits/order_cubit/order_state.dart
@@ -4,3 +4,15 @@ part of 'order_cubit.dart';
 sealed class OrderState {}
 
 final class OrderInitial extends OrderState {}
+
+final class OrderAddingStatus extends OrderState {}
+
+final class OrderAddedSuccess extends OrderState {
+  final String successMessage;
+  OrderAddedSuccess(this.successMessage);
+}
+
+final class OrderAddedError extends OrderState {
+  final String errMessage;
+  OrderAddedError(this.errMessage);
+}
